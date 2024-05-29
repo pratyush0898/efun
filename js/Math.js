b9249f5670ff34417fe4f2dc98a88c27bac8004a
Pratyush 2nd github <155731201+pratyush0898@users.noreply.github.com>
2024-05-29 18:27:47 +0530

    Create Math.js

---
+++ js/Math.js
@@ -0,0 +1,64 @@
+export function aum(a, b) {
+    return a + b;
+}
+
+export function sub(a, b) {
+    return a - b;
+}
+
+export function mul(a, b) {
+    return a * b;
+}
+
+export function div(a, b) {
+    return a / b;
+}
+
+export function pow(a, b) {
+    return Math.pow(a, b);
+}
+
+export function sqrt(a) {
+    return Math.sqrt(a);
+}
+
+export function sin(a) {
+    return Math.sin(a);
+}
+
+export function cos(a) {
+    return Math.cos(a);
+}
+
+export function tan(a) {
+    return Math.tan(a);
+}
+
+export function asin(a) {
+    return Math.asin(a);
+}
+
+export function acos(a) {
+    return Math.acos(a);
+}
+
+export function atan(a) {
+    return Math.atan(a);
+}
+
+export function sinh(a) {
+    return Math.sinh(a);
+}
+
+export function cosh(a) {
+    return Math.cosh(a);
+}
+
+export function tanh(a) {
+    return Math.tanh(a);
+}
+
+
+export function log(a, b) {
+    return Math.log(a) / Math.log(b);
+}
