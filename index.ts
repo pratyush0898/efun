fe8c576d8abe3a51906f7aed66744b55ae6d3ade
Pratyush 2nd github <155731201+pratyush0898@users.noreply.github.com>
2024-06-05 10:54:53 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -105,7 +105,7 @@ interface GreetFunction {
 
 const json = {
   "name": "efun",
-  "version": "1.2.0",
+  "version": "1.3.0",
   "description": "A collection of extra utility functions to enhance your JavaScript projects.",
   "main": "index.ts",
   "scripts": {
